58d61b37c1d9951c833f0b1a7ca914664b00a9c4
iamraselmolla <raselmolla6336@gmail.com>
2022-11-08 11:56:41 +0600

    Showing only 3 packages for homepage setting data limit in Database

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const app = express()
-app.use(cors())
-app.use(express.json())
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q37bxqk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -14,14 +14,14 @@ async function run() {
         app.get('/home', async (req, res) => {
             const query =  {};
             const cursor  = packagesCollection.find(query).limit(3);
-            const allData = await cursor.toArray()
-            res.send(allData)
+            const allData = await cursor.toArray();
+            res.send(allData);
         })
         app.get('/packages', async (req, res) => {
             const query =  {};
             const cursor  = packagesCollection.find(query);
-            const allData = await cursor.toArray()
-            res.send(allData)
+            const allData = await cursor.toArray();
+            res.send(allData);
         })
     }
     finally {

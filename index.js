be6ad19d4b75ef74c58537c3e3ac1f7027c3bfa9
iamraselmolla <raselmolla6336@gmail.com>
2022-11-08 11:37:29 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+app.use(cors())
+app.use(express.json())
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

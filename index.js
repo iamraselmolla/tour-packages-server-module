31717d1075dce82132bef05308bdaf00073cbb5b
iamraselmolla <raselmolla6336@gmail.com>
2022-11-10 02:14:49 +0600

    Added jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -8,6 +9,20 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q37bxqk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+       return res.status(401).send({message: 'Unauthorized access 1'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err,decoded){
+        if(err){
+           return res.status(401).send({message: 'Unauthorized access 2'})
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
 async function run() {
     try {
         const packagesCollection = client.db("tour").collection("packages")
@@ -18,19 +33,25 @@ async function run() {
             const allData = await cursor.toArray();
             res.send(allData);
         })
-        app.get('/packages', async (req, res) => {
+        app.post('/jwt', (req, res)=> {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+            res.send({token})
+
+        })
+        app.get('/services', async (req, res) => {
             const query = {};
             const cursor = packagesCollection.find(query);
             const allData = await cursor.toArray();
             res.send(allData);
         })
-        app.get('/packages/:id', async (req, res) => {
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const cursor = await packagesCollection.findOne(query);
             res.send(cursor)
         })
-        app.post('/packages', async (req, res) => {
+        app.post('/services', async (req, res) => {
             const data = req.body;
             const result = await packagesCollection.insertOne(data);
             res.send(result);
@@ -42,6 +63,7 @@ async function run() {
 
         })
         app.get('/post-review/:id', async (req, res) => {
+            
             const id = req.params.id
             const query = { packageId: id }
             const cursor = reviewsCollection.find(query);
@@ -63,7 +85,6 @@ async function run() {
         app.put('/edit-comment/:id', async (req, res) => {
             const id = req.params.id;
             const status = req.body;
-            console.log(status)
             const query = {_id : ObjectId(id)};;
             const updatedDoc = {
                 $set:{
@@ -72,6 +93,22 @@ async function run() {
             }
             const result = await reviewsCollection.updateOne(query, updatedDoc);
             res.send(result)
+        })
+        app.get('/my-review', async(req,res) => {
+          
+        //     if(req.decoded.email !== req.query.email){
+        //         res.status(401).send({message: 'Unauthorized Access 3'})
+             
+        //    }
+            let query = {};
+            if(req.query.email){
+                query = {email: req.query.email}
+            }
+            const cursor = reviewsCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result);
+
+
         })
     }
     finally {

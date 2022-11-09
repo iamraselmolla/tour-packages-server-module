28d9e2b36cae4fafaf463d620ae95bd828a2fafc
iamraselmolla <raselmolla6336@gmail.com>
2022-11-09 16:45:03 +0600

    added deleting api method

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
+        app.delete('/post-review/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)}
+            const result = await reviewsCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
         console.log()

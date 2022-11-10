75b6cd9a8aebdea0664a4369bab7a3202036bc46
iamraselmolla <raselmolla6336@gmail.com>
2022-11-10 13:21:25 +0600

    added my service api and verified jwt

--- index.js
+++ index.js
@@ -9,20 +9,24 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q37bxqk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Verify JWT
 function verifyJWT(req, res, next){
     const authHeader = req.headers.authorization;
-    if(!authHeader){
-       return res.status(401).send({message: 'Unauthorized access 1'})
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
     }
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err,decoded){
-        if(err){
-           return res.status(401).send({message: 'Unauthorized access 2'})
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
         }
         req.decoded = decoded;
-        next()
+        next();
     })
 }
+
 async function run() {
     try {
         const packagesCollection = client.db("tour").collection("packages")
@@ -35,12 +39,22 @@ async function run() {
         })
         app.post('/jwt', (req, res)=> {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5d' });
             res.send({token})
 
         })
         app.get('/services', async (req, res) => {
             const query = {};
+            const cursor = packagesCollection.find(query);
+            const allData = await cursor.toArray();
+            res.send(allData);
+        })
+        app.get('/added-services', async (req, res) => {
+            let query = {};
+            if(req.query.email){
+                query = {email: req.query.email}
+            }
             const cursor = packagesCollection.find(query);
             const allData = await cursor.toArray();
             res.send(allData);
@@ -51,7 +65,7 @@ async function run() {
             const cursor = await packagesCollection.findOne(query);
             res.send(cursor)
         })
-        app.post('/services', async (req, res) => {
+        app.post('/services', verifyJWT, async (req, res) => {
             const data = req.body;
             const result = await packagesCollection.insertOne(data);
             res.send(result);
@@ -94,12 +108,14 @@ async function run() {
             const result = await reviewsCollection.updateOne(query, updatedDoc);
             res.send(result)
         })
-        app.get('/my-review', async(req,res) => {
+        app.get('/my-review', verifyJWT, async(req,res) => {
           
-        //     if(req.decoded.email !== req.query.email){
-        //         res.status(401).send({message: 'Unauthorized Access 3'})
-             
-        //    }
+            const decoded = req.decoded;
+            console.log(decoded)
+
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' })
+            }
             let query = {};
             if(req.query.email){
                 query = {email: req.query.email}
